1389b6e8dacbacbad12d80f8678e6e493baca813
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-08 21:28:17 -0500

    push

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const SECTIONS = [
-   { name:"Soccer", color: "green"},
-   { name:"Basketball"},
-   { name:"Football"},
-   { name:"F1"},
-   { name:"Tennis"},
-   { name:"Rugby"},
+   { name:"Soccer", color: "fefae0"},
+   { name:"Basketball", color:"f18701"},
+   { name:"Football", color:"003e1f"},
+   { name:"F1", color: "d9d9d9"},
+   { name:"Tennis", color: "bfd200"},
+   { name:"Rugby", color:"6a040f"},
 ];
