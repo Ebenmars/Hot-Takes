868f2f04e7a937133de8cd711240e7989143357a
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-12-11 00:23:32 -0500

    Time is now working

--- data.js
+++ data.js
@@ -5,3 +5,25 @@ const SECTIONS = [
    { name:"F1", color: "d9d9d9"},
    { name:"Rugby", color:"6a040f"},
 ];
+
+
+const defaultPosts = [
+   {
+      id:1,
+      title: "Ronaldo is the best soccer player in history",
+      created_at: new Date(),
+      description: "Cristiano Ronaldo is widely regarded as one of the greatest soccer players of his generation. His goal-scoring ability, athleticism, and versatility have earned him numerous awards and accolades throughout his career. ",
+      category: "Soccer",
+      thumbsUp: 20,
+      thumbsDown: 10
+   },
+   {
+      id:2,
+      title: "Lebron has the most points in NBA history",
+      created_at: new Date(),
+      description: "LeBron James is known for his scoring ability, versatility, and overall impact on the game, and he continues to be a prominent figure in the NBA.",
+      category: "Basketball",
+      thumbsUp: 20,
+      thumbsDown: 10
+   },
+]
--- script.js
+++ script.js
@@ -1,13 +1,71 @@
 
 const create_btn = document.querySelector("#create-post");
-const form = document.querySelector(".post-form")
+const form = document.querySelector(".post-form");
+const postList = document.querySelector("#posts-list");
+
+
+
 create_btn.addEventListener("click",function () {
-    
+    postList.innerHTML = "";
     if(form.classList.contains("hidden")){
         form.classList.remove("hidden");
+        create_btn.textContent = "x";
     }
     else{
         form.classList.add("hidden");
+        create_btn.textContent = "+";
     }
-   
+
 })
+
+postList.innerHTML = "";
+
+
+//Load data from Supabase
+loadPosts();
+
+
+async function loadPosts() {
+
+    //pause exectution
+    const res = await fetch("https://acnuilrhdcvfmphsdyog.supabase.co/rest/v1/posts", {
+        headers: {
+            apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjbnVpbHJoZGN2Zm1waHNkeW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyNDUzNjEsImV4cCI6MjAxNzgyMTM2MX0.UEVjLVN5KRa3dhr17ay4bYIzzcq1-ogh703z5z7w0sc",
+            authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjbnVpbHJoZGN2Zm1waHNkeW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyNDUzNjEsImV4cCI6MjAxNzgyMTM2MX0.UEVjLVN5KRa3dhr17ay4bYIzzcq1-ogh703z5z7w0sc",
+        },
+    });
+
+    const data = await res.json()
+    createPostsList(data);
+    console.log(data);
+
+}
+
+function createPostsList(dataArr) {
+    const htmlArr = dataArr.map((post) => {
+        // Create a new Date object from the timestamp
+        var date = new Date(post.created_at);
+
+        // Format the date and time
+        var formattedDate = date.toLocaleDateString();
+        var formattedTime = date.toLocaleTimeString("en-US");
+
+        return `<li class="post-on-feed">
+            <h6>
+                ${post.title}
+            </h6>
+            <span>${formattedDate} ${formattedTime}</span>
+            <span class="tag" style="background-color: #fefae0; color: #000000;">${post.description}</span>
+            <div class="vote-buttons">
+                <button><i class="fa-regular fa-thumbs-up"></i> <strong>100</strong></button>
+                <button><i class="fa-regular fa-thumbs-down"></i> <strong>14</strong></button>
+            </div>
+        </li>`
+    });
+    const postsContainer = document.getElementById('posts-list');
+    postsContainer.innerHTML = htmlArr.join('');
+}
+
+
+
+
